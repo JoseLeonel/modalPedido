aba88781eb429f838c3fbdc6dfc22837bfa36d86
magui <magui.cerisola@gmail.com>
2023-03-07 16:13:50 -0300

    styles button and responsive

--- index.js
+++ index.js
@@ -1,22 +1,11 @@
-const menuButton = document.querySelector(".js-burger");
-const nav = document.querySelector(".js-nav");
 const menuButtonComment = document.querySelector(".js-burger-comment");
 const navComment = document.querySelector(".js-nav-comment");
 
-menuButton.onclick = toggleMenu;
 menuButtonComment.onclick= toggleMenuComment
 
-menuButton.onclick = function(e) {
-  e.stopPropagation();
-  toggleMenu()
-}
 menuButtonComment.onclick = function(e) {
   e.stopPropagation();
   toggleMenuComment()
-}
-
-nav.onclick = function (e) {
-  e.stopPropagation();
 }
 
 navComment.onclick = function (e) {
@@ -24,17 +13,9 @@ navComment.onclick = function (e) {
 }
  
 document.body.onclick = function () {
-  nav.classList.remove("open");
   navComment.classList.remove("open");
 }
 
-function toggleMenu() {
-  if(nav.classList.contains("open")) {
-    nav.classList.remove("open");
-  }else{
-    nav.classList.add("open");
-  }
-}
 
 function toggleMenuComment() {
   if(navComment.classList.contains("open")) {
@@ -43,6 +24,7 @@ function toggleMenuComment() {
     navComment.classList.add("open");
   }
 }
+
 
 
 
